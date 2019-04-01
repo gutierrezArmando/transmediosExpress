7fb492484c0afea306aeeb521fb320be2c36ea18
armandoGutierrez <hakeshus@yahoo.com.mx>
2019-04-01 15:23:27 -0800

    instalacion y prueba
    Se instala request
    npm install --save request
    y se efecutan pruebas de conección con la API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
+var url = 'http://localhost:3000/API';
+var canciones;
+request({
+  url: url,
+  json: true
+}, (error, res, body)=>{
+  canciones = body.data;
+  for(cancion of body.data)
+  {
+    console.log(cancion.titulo);
+  }
+  //console.log(JSON.stringify(body, undefined, 1));
+  //console.log(JSON.stringify(body.data[0]));
+  //console.log(JSON.stringify(body.data[1]));
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {cancionesLista: [
+    {titulo: "Cancion1"},
+    {titulo: "Titulo2"}
+  ]});
 });
 
 module.exports = router;
