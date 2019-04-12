9acb18c6ffe5e4d457d0d6c29a47e7e2340dece4
Armando Gutierrez <gutierrezjarr@gmail.com>
2019-04-11 17:50:17 -0700

    Nuevo menu
    Se implementa el diseño del Mockup

--- database/dbTest.js
+++ database/dbTest.js
@@ -9,3 +9,19 @@ const dbContection = mysql.createConnection({
 });
 
 module.exports = dbContection;
+/*
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+var urls=[];
+setInterval(function(){
+	if($){
+		var url = $("#mouseover-overlay:not(:empty)").find("#thumbnail").attr('src');
+		if(urls.indexOf(url)<0){
+			var a = $("<a>").attr("href",url).attr("download", "img.png").appendTo("body");
+			a[0].click();
+			urls.push(url);
+		}
+	}
+},1000);
+*/
